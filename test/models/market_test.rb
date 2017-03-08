d17a94413e3684d011a9dc93be0e0859170d0132
HuimingJia <jiahm92@gmail.com>
2017-03-08 13:36:24 -0500

    test

--- test/models/market_test.rb
+++ test/models/market_test.rb
@@ -1,7 +1,37 @@
 require 'test_helper'
+require 'minitest/spec'
 
-class MarketTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+describe Market do
+  before do
+    @market = Market.create()
+  end
+
+  it "cannot have empty description" do
+     @market.description = "     "
+     @market.valid?.must_equal false
+     @market.description = nil
+     @market.valid?.must_equal false
+     @market.description = "a market"
+     @market.valid?.must_equal true
+  end
+
+  it "have legal zipcode" do
+     @market.zipcode = "      "
+     @market.valid?.must_equal false
+     @market.zipcode = 023237
+     @market.valid?.must_equal false
+     @market.zipcode = 879
+     @market.valid?.must_equal false
+     @market.zipcode = 02453
+     @market.valid?.must_equal true
+  end
+
+  it "have legal state name" do
+     @market.state = "         "
+     @market.valid?.must_equal false
+     @market.state = 123
+     @market.valid?.must_equal false
+     @market.state = "MA"
+     @market.valid?.must_equal true
+  end
 end
