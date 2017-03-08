e75a97451e628debeea4432e8e65b0cb12a83105
Beraa Ibas <bibas@brandeis.edu>
2017-03-08 13:33:02 -0500

    store model tests

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,4 +1,8 @@
 class Store < ApplicationRecord
-  belongs_to :market
-  belongs_to :vendor
+  # belongs_to :market
+  # belongs_to :vendor
+
+  validates :description,  presence: true, length: { maximum: 500 }
+  validates :open_time, presence: true, length: { maximum: 8}
+  validates :open_time, presence: true, length: { maximum: 8}
 end
--- test/models/store_test.rb
+++ test/models/store_test.rb
@@ -1,7 +1,40 @@
 require 'test_helper'
+require "minitest/autorun"
 
 class StoreTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @store = Store.new(description: "Example Description", open_time: "8:00am", close_time: "6:00pm")
+  end
+
+  test "should be valid" do
+    assert @store.valid?
+  end
+
+  test "name should be present" do
+    @store.description = ""
+    assert_not @store.valid?
+  end
+
+  test "email should be present" do
+    @store.description = "     "
+    assert_not @store.valid?
+  end
+
+  describe Store do
+    before do
+      @store = Store.new(description: "Example Description", open_time: "8:00am", close_time: "6:00pm")
+    end
+
+    it "has non-empty description" do
+      @store.description.wont_equal ""
+    end
+
+    it "has non-empty open_time" do
+      @store.open_time.wont_equal ""
+    end
+
+    it "has non-empty close_time" do
+      @store.close_time.wont_equal ""
+    end
+  end
 end
