16174f1b2e475844c4cd577e357301ef880aa61b
HuimingJia <jiahm92@gmail.com>
2017-03-22 15:10:25 -0400

    I dont know what I have done

--- app/models/market.rb
+++ app/models/market.rb
@@ -1,3 +1,6 @@
 class Market < ApplicationRecord
   has_many :stores
+  validates :description,  presence: true, length: { maximum: 500 }
+  validates :open_time, presence: true, length: { maximum: 8}
+  validates :open_time, presence: true, length: { maximum: 8}
 end
--- app/models/store.rb
+++ app/models/store.rb
@@ -1,6 +1,6 @@
 class Store < ApplicationRecord
-  # belongs_to :market
-  # belongs_to :vendor
+  belongs_to :market
+  belongs_to :vendor
 
   validates :description,  presence: true, length: { maximum: 500 }
   validates :open_time, presence: true, length: { maximum: 8}
