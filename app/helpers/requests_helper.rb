24e1b6d0fb3e3ea39e32179a6dc66b4fa821cb0f
HuimingJia <jiahm92@gmail.com>
2017-04-27 22:29:36 -0400

    implement request

--- app/helpers/requests_helper.rb
+++ app/helpers/requests_helper.rb
@@ -1,2 +1,37 @@
 module RequestsHelper
+  def translate_status_to_market_owner(status)
+    if status == 0
+      "Unread"
+    elsif status == 1
+      "Readed"
+    elsif status == 2
+      "Refused"
+    elsif status == 3
+      "Refused"
+    elsif status == 4
+      "Accepted"
+    elsif status == 5
+      "Accepted"
+    elsif status == 6
+      "Accepted"
+    end
+  end
+
+  def translate_status_to_request_sender(status)
+    if status == 0
+      "Unread"
+    elsif status == 1
+      "Readed"
+    elsif status == 2
+      "Refused"
+    elsif status == 3
+      "Refused"
+    elsif status == 4
+      "Accepted"
+    elsif status == 5
+      "Accepted"
+    elsif status == 6
+      "Accepted"
+    end
+  end
 end
