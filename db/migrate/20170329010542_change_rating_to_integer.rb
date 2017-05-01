93e640a17c1a242f227082174673163c977543c3
HuimingJia <jiahm92@gmail.com>
2017-04-30 23:18:41 -0400

    deploy

--- db/migrate/20170329010542_change_rating_to_integer.rb
+++ db/migrate/20170329010542_change_rating_to_integer.rb
@@ -3,7 +3,7 @@ class ChangeRatingToInteger < ActiveRecord::Migration[5.0]
     # change_column :markets, :rating, :integer
     # change_column :reviews, :rating, :integer
 
-    change_column :markets, :rating, 'integer USING CAST(open_time AS integer)'
-    change_column :reviews, :rating, 'integer USING CAST(open_time AS integer)'
+    change_column :markets, :rating, 'integer USING CAST(rating AS integer)'
+    change_column :reviews, :rating, 'integer USING CAST(rating AS integer)'
   end
 end
