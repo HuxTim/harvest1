a340dbd064764a67acfac830a7dc8dd5c7b59c7e
huxtim <huxtim@brandeis.edu>
2017-05-06 15:40:42 -0400

    finished mailing algo

--- app/mailers/subscription_mailer.rb
+++ app/mailers/subscription_mailer.rb
@@ -7,15 +7,68 @@ class SubscriptionMailer < ApplicationMailer
 
   #receives an array of user models
   def send_emails()
-    #variables
-    #algorithm
-    5.times do
-      SubscriptionMailer.subscription_digest().deliver_now
+    #gather all users from subscriptions DB *users*
+    users = []
+    Subscription.all.each do |sub|
+      if !users.include? sub.user_id
+        users.push sub.user_id
+      end
     end
+    #for each user with subscriptions
+    users.each do |user|
+      products = []
+      sCount = 0
+      pCount = 0
+      #go through the stores where they are subscribed at
+      (Subscription.where(user_id: user.id).sort_by {rand}).each do |sub|
+        #go through products of store
+        specials = (Store.where(vendor_id: sub.vendor_id).first.products.where(is_special: true).sort_by {rand})
+        while pCount <= 2 and !specials.empty? do
+          product = specials.pop
+          if product.updated_at > sub.updated_at
+            products.push product
+            sub.updated_at = Time.now
+            sCount = sCount + .5
+          end
+        end
+        pCount = 0
+        sCount = sCount.round
+
+        if sCount = 3
+          SubscriptionMailer.subscription_digest(user, products).deliver_now
+        end
+      end
+        if sCount > 0
+          while sCount < 3
+            randomRecommendations = Product.where(is_special: true).sample.store.products
+            if randomRecommendations.count > 1
+            products.merge *randomRecommendations.sample(2)
+            else
+            products.push randomRecommendations.sample
+            end
+            sCount = sCount + 1
+          end
+          SubscriptionMailer.subscription_digest(user, products).deliver_now
+        end
+    end
+
+    #for loop for all users
+      #*products*
+      #go through each subscription for that user
+      #subscription.where(user_id: id).sort_by {rand}
+      #store count variable
+      #for each subscription, look for 2 items from that vendor
+      #if reach 3 stores send email
+      #if reach end send email if products has more than 2 products
+      #do nothing
+    #dont forget to update the subscription if it gets added
+    #algorithm
   end
 
-  def subscription_digest()
-      mail to: "HarvestApp1@gmail.com", subject: "Harvest: Your Weekly Specials"
+  def subscription_digest(user, products)
+    @products = products
+    @user = user
+      mail to: user.email, subject: "Harvest: Check out these CRAZY Deals at your Local Farmers Markets!"
   end
 
   private
