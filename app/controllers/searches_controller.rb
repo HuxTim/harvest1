06fc67247c7fb609a57a9822b32a94a42643cef2
huxtim <huxtim@brandeis.edu>
2017-05-08 22:56:05 -0400

    distance sort algorithm

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -91,6 +91,111 @@ class SearchesController < ApplicationController
     def search_params
       params.fetch(:search, {})
     end
+
+    #input products list; output [product distance] in order of distance
+    def dst_SortProducts(products)
+
+      distances = []
+      distProd = Hash.new
+
+      products.each do |prod|
+        if !distances.include? p_calculateDistance(prod)
+          distances.push p_calculateDistance(prod)
+          distProd[p_calculateDistance(prod)] = []
+        end
+        distProd[p_calculateDistance(prod)] = distProd[p_calculateDistance(prod)].push prod
+      end
+
+      distances.each do |d|
+        distProd[d].sort!
+      end
+
+      distances.sort!
+      sortedProducts = []
+      distances.each do |d|
+        sortedProducts.push *distProd[d]
+      end
+      return sortedProducts
+    end
+
+    #input products list; output [product distance] in order of distance
+    #same algo as above
+    def dst_SortStores(stores)
+      distances = []
+      distStores = Hash.new
+
+      stores.each do |str|
+        if !distances.include? p_calculateDistance(str)
+          distances.push p_calculateDistance(str)
+          distStores[p_calculateDistance(str)] = []
+        end
+        distStores[p_calculateDistance(str)] = distStores[p_calculateDistance(str)].push str
+      end
+
+      distances.each do |d|
+        distStores[d].sort!
+      end
+
+      distances.sort!
+      sortedStores = []
+      distances.each do |d|
+        sortedStores.push *distStores[d]
+      end
+      return sortedStores
+    end
+
+    #input products list; output [product distance] in order of distance
+    #same algo as above
+    def dst_SortMarkets(markets)
+      distances = []
+      distMark = Hash.new
+
+      markets.each do |mkt|
+        if !distances.include? p_calculateDistance(mkt)
+          distances.push p_calculateDistance(mkt)
+          distMark[p_calculateDistance(mkt)] = []
+        end
+        distMark[p_calculateDistance(mkt)] = distProd[p_calculateDistance(mkt)].push mkt
+      end
+
+      distances.sort!
+      sortedMarkets = []
+      distances.each do |d|
+        sortedMarkets.push *distMark[d]
+      end
+      return sortedMarkets
+    end
+
+    def p_calculateDistance(product)
+      distance = 12425 #fun trvia, farthest distance two cities can be in the world
+      #returns the most closest market
+      store.markets.each do |market|
+        if Geocoder::Calculations.distance_between(Geocoder.coordinates(market.address + market.city + market.state), Geocoder.coordinates(remote_ip)) < distance#request.remote_ip instead of remote_ip
+          distance = Geocoder::Calculations.distance_between(Geocoder.coordinates(market.address + market.city + market.state), Geocoder.coordinates(remote_ip))
+        end
+      end
+      return distance
+    end
+
+    def s_calculateDistance(store)
+      distance = 12425
+      product.store.markets.each do |market|
+        if Geocoder::Calculations.distance_between(Geocoder.coordinates(market.address + market.city + market.state), Geocoder.coordinates(remote_ip)) < distance#request.remote_ip instead of remote_ip
+          distance = Geocoder::Calculations.distance_between(Geocoder.coordinates(market.address + market.city + market.state), Geocoder.coordinates(remote_ip))
+        end
+      end
+      return distance
+    end
+
+    def m_calculateDistance(market)
+      distance = 12425
+      markets.each do |market|
+        if Geocoder::Calculations.distance_between(Geocoder.coordinates(market.address + market.city + market.state), Geocoder.coordinates(remote_ip)) < distance#request.remote_ip instead of remote_ip
+          distance = Geocoder::Calculations.distance_between(Geocoder.coordinates(market.address + market.city + market.state), Geocoder.coordinates(remote_ip))
+        end
+      end
+      return distance
+    end
 end
 
 def preprocess(query)
